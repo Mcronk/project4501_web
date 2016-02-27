55ea160b7bc7dc3a4d1463bb0cd458d3ef572c35
Sam <bs5sk@virginia.edu>
2016-02-27 10:35:32 -0500

    Set up all the views

--- project4501_web/settings.py
+++ project4501_web/settings.py
@@ -1,9 +1,8 @@
+
 """
 Django settings for project4501_web project.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
-
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
--- project4501_web/urls.py
+++ project4501_web/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +8,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    #url(r'^/(?P<pk>[0-9]+)/$', views.index, name='index'),
+    url(r'^product/$', views.product, name='product'),
+    url(r'^product/(?P<pk>[0-9]+)/$', views.product, name='product'),
 )
