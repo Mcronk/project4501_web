8aac20ad3c3dc32be8bbac6aa9df07b550a0d061
Sam <bs5sk@virginia.edu>
2016-02-27 11:22:32 -0500

    Second commit

--- project4501_web/views.py
+++ project4501_web/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import urllib.request
+import urllib.parse
+import json
+
+# make a GET request and parse the returned JSON                                                                                                                                                           # note, no timeouts, error handling or all the other things needed to do this for real                                                                                                                      
+
+
 
 def index(request):
     return render(request, 'index.html')
 
 def product(request, pk = ''):
     return render(request, 'product.html')
+
+    
