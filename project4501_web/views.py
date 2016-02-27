ca3e4cf5c5f7bdc66bd0c35fa884545021230f92
Sam <bs5sk@virginia.edu>
2016-02-27 12:09:17 -0500

    Third message

--- project4501_web/views.py
+++ project4501_web/views.py
@@ -3,15 +3,23 @@ from django.shortcuts import render
 import urllib.request
 import urllib.parse
 import json
+#import requests
 
 # make a GET request and parse the returned JSON                                                                                                                                                           # note, no timeouts, error handling or all the other things needed to do this for real                                                                                                                      
 
-
-
 def index(request):
-    return render(request, 'index.html')
+	return render(request, 'index.html')
 
 def product(request, pk = ''):
-    return render(request, 'product.html')
+	#req = requests.get('http://www.google.com')
+	#req = requests.get('http://localhost:8002/product')
+	#json_data = r.json()
 
-    
+
+	req = urllib.request.Request('http://exp-api:8000/product')
+	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+	resp = json.loads(resp_json)
+	context_dict = resp 
+	return render(request, 'product.html', context_dict)
+
+	
