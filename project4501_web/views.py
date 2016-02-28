15768fba3784b95589f49b7b77ebc3a57debc142
Johnson <johnson.wuyou@gmail.com>
2016-02-28 16:01:39 -0500

    updated views

--- project4501_web/views.py
+++ project4501_web/views.py
@@ -10,11 +10,11 @@ def home(request):
 	return render(request, 'home.html')
 
 def course_info(request, pk = ''):
-	req = urllib.request.Request('http://exp-api:8000/product')
+	req = urllib.request.Request('http://exp-api:8000/product'+pk)
 	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
 	resp = json.loads(resp_json)
 	context_dict = resp
 	context_dict2 = {'name': 'vainglory', 'tutor': 'johnson', 'price': '333', 'description': 'Teach you how to be a master in vainglory in three classes, each with 60 minutes.'}
-	return render(request, 'course_info.html', context_dict2)
+	return render(request, 'course_info.html', context_dict)
 
 	
